467001f32474f47b6f85514551b74da7ddc71b82
kessiler <keessiler@hotmail.com>
2014-09-13 01:43:58 -0300

    removed unusable variable

--- source/Core/AntiPublicistas.cs
+++ source/Core/AntiPublicistas.cs
@@ -10,8 +10,6 @@ namespace Cyber.Core
 {
 	internal class AntiPublicistas
 	{
-        internal static Regex reg = new Regex("[^a-zA-Z0-9 ]");
-
         internal static string[] bannedHotels;
 
 		internal static void Load(IQueryAdapter DBClient)
@@ -34,10 +32,7 @@ namespace Cyber.Core
 			{
 				return false;
 			}
-            // Quitar espacios y caracteres extraños.
             Message = Message.ToLower().Replace(" ", "").Replace("\u00a0", "").Replace("/", "").Replace("-", "").Replace("_", "").Replace("*", "").Replace("0", "o").Replace("4", "a").Replace("1", "i");
-
-
             return bannedHotels.Any(Message.Contains) && !Message.Contains("ban") && !Message.Contains("mip");
 		}
 	}
